ef026bb7394f85b7608bfaf4fde8e68adaae8834
Owen McGann <owenmcgann@seattleacademy.org>
2021-05-03 13:22:19 -0700

    Add drive command

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 1522
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/drive', (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+	console.log(req.query,req.body);
+  res.send("i'm driving!")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://math.seattleacademy.org/${port}`)
+})
